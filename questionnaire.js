b3504dba365a68aa58bc6a2f47d7bd589cb0d2cb
meghanathambi <challanarendra3003@gmail.com>
2025-07-29 21:56:21 +0530

    Delete questionnaire.js

--- questionnaire.js
+++
@@ -1,5 +0,0 @@
-function showSchemes() {
-  const interSelected = document.getElementById('Inter').checked;
-  const schemeSection = document.getElementById('schemeOptions');
-  schemeSection.style.display = interSelected ? 'block' : 'none';
-}
